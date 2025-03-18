c0c1f698c88b3ae0ef66575c6293d29715d82bc4
DhruTesh <dhruteshdev5@gmail.com>
2025-03-18 19:32:34 +0530

    NavBar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 // Folder: /pages/index.tsx
 import JobList from '../components/JobList';
 import "../app/globals.css";
+import Nav from '@/components/Nav';
 
-export default function Home() {
-  return <JobList />;
+
+const HomePage = () => {
+  return (
+    <div className="overflow-hidden">
+      <Nav />
+      <JobList />
+    </div>
+  )
 }
+
+export default HomePage
